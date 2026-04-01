fbdb69842ac1d67e956edb6565cbd604593c1794
Нариман Кусаинов <narimankussainov@gmail.com>
2026-04-01 23:04:09 +0500

    Добавил веб-заглушку для бесплатного Render

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import asyncio
 import logging
+import os
+from aiohttp import web
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from database import init_db
@@ -7,6 +9,9 @@ from handlers import router
 
 logging.basicConfig(level=logging.INFO)
 
+# Крошечный веб-сервер для обмана Render (заглушка)
+async def health_check(request):
+    return web.Response(text="Бот жив и работает!")
 
 async def main():
     # 1. Инициализируем базу данных
@@ -15,14 +20,22 @@ async def main():
     # 2. Создаем бота и диспетчер
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
-
-    # 3. Подключаем наши хэндлеры к диспетчеру
     dp.include_router(router)
+
+    # 3. --- ЗАПУСК ЗАГЛУШКИ ДЛЯ RENDER ---
+    app = web.Application()
+    app.router.add_get('/', health_check)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    # Render сам выдаст нам порт, мы его ловим
+    port = int(os.environ.get("PORT", 8000))
+    site = web.TCPSite(runner, '0.0.0.0', port)
+    await site.start()
+    # -------------------------------------
 
     print("Бот успешно запущен...")
     # 4. Запускаем бота
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
